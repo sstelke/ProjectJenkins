dd7522b9568082859e355cebba5e4566131554eb
sstelke <telkelaxmikant@gmail.com>
2019-12-05 20:20:36 +0000

    Adding latest calculator

--- calculator.cpp
+++ calculator.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main()
 {
     int x=20,y=10;
-    
+
     cout<<"x:"<<x<<"y:"<<y<<"\n";
-    cout<<"Addition:"<<x+y<<"\n";
-    cout<<"Subtraction:"<<x-y<<"\n";
-    cout<<"Division:"<<x/y<<"\n";
+    cout<<"Addition is:"<<x+y<<"\n";
+    cout<<"Subtraction is:"<<x-y<<"\n";
+    cout<<"Division is:"<<x/y<<"\n";
     cout<<"Multiplication:"<<x*y<<"\n";
     return 0;
 }
