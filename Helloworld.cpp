e3bb2b1b3090bb6c404369add1a31a15f19c8aa0
sstelke <telkelaxmikant@gmail.com>
2019-12-05 19:45:56 +0000

    Calculator code adding

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main()
 {
-    cout<<"Hello World Telusko";
+    cout<<"Hello World Telusko\n";
   return 0;
 }
---
+++ calculator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cin>>x;
+    cin>>y;
+    cout<<"x:"<<x<<"y:"<<y<<"\n";
+    cout<<"Addition:"<<x+y<<"\n";
+    cout<<"Subtraction:"<<x-y<<"\n";
+    cout<<"Division:"<<x/y<<"\n";
+    cout<<"Multiplication:"<<x*y<<"\n";
+    return 0;
+}
